d6f40ffc707fa753f968198c945cee9808e51fd4
Jaime Montoya <innovasistemas@gmail.com>
2025-11-13 01:06:44 -0500

    Función de Ackermann

--- com/packages/recursivity/Recursion.java
+++ com/packages/recursivity/Recursion.java
@@ -74,4 +74,16 @@ public class Recursion
             return (int)Math.pow(n, 2) + sumSquare(n - 1);
         }
     }
+
+
+    public int Ackermann(int m, int n)
+    {
+        if (m == 0) {
+            return n + 1;
+        } else if (n == 0) {
+            return Ackermann(m - 1, 1);
+        } else {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
 }
